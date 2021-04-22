46985a866e1690832ac430b544784c074fd244b7
Nurul Amin Shakil <na.shakil1995@gmail.com>
2021-04-23 03:59:37 +0600

    added menu

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+</head>
+<body>
+
+ @include('layout.menu')
+
+@yield('content')
+
+
+ @include('layout.footer')
+    <script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('jsbootstrap.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/',[SiteController :: class,'showHome']);
+Route::get('/about',[SiteController :: class,'showAbout']);
+Route::get('/service',[SiteController :: class,'showServices']);
+Route::get('/portfolio',[SiteController :: class,'showPortfolio']);
 
 
